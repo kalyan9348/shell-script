a16ffdcc0f37559e8263e10456751eb807334608
Kalyan Reddy <kalyanreddy5030@gmail.com>
2023-12-12 22:59:57 +0530

    added 2nd script

--- 03-variables.sh
+++ 03-variables.sh
@@ -5,3 +5,10 @@ echo "Script started executing: ${DATE}"
 USERNAME=KALYAN
 PASSWORD=KALYAN123123
 echo "Username is:$USERNAME, Password is:$PASSWORD"
+
+echo "Please enter your username:: "
+
+read -s USERNAME #the value entered above will be automatically atteched to USERNAME variable
+
+echo "Please enter your Password:: "
+read -s PASSWORD
