967fbd9bd7cfb77d82660b18aa6c75d2044fb963
Kalyan Reddy <kalyanreddy5030@gmail.com>
2023-12-16 10:42:45 +0530

    added 2nd script

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -9,6 +9,14 @@ echo "fourth value: ${fruits[3]}"
 
 names=("kalyan" "saleem" "akhil" "sharma" "rohith")
 
+echo "Kalyan friend is: ${names[1]}"
+echo "Saleem friend is: ${names[2]}"
+echo "akhil friend is: ${names[3]}"
+echo "sharma friend is: ${names[4]}"
+echo "rohith friend is: ${names[0]}"
+
+names=("kalyan" "saleem" "akhil" "sharma" "rohith")
+
 echo "Kalyan friend is: ${names[0]}"
 echo "Saleem friend is: ${names[1]}"
 echo "akhil friend is: ${names[2]}"
