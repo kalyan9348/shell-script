6cfabd0549ac91de0be9c794fad568e585f375d5
Kalyan Reddy <kalyanreddy5030@gmail.com>
2023-12-16 10:08:27 +0530

    added 2nd script

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -1,6 +1,6 @@
 fruits=("apple" "guava" "banana" "grapes")
 
-echo "first value: $fruits[0]"
-echo "second value: $fruits[1]"
-echo "third value: $fruits[2]"
-echo "fourth value: $fruits[3]"
+echo "first value: $(fruits[0])"
+echo "second value: $(fruits[1])"
+echo "third value: $(fruits[2])"
+echo "fourth value: $(fruits[3])"
