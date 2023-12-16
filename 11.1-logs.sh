588abd33fe3c4d007db59229fd80918c9a357c55
Kalyan Reddy <kalyanreddy5030@gmail.com>
2023-12-16 22:16:00 +0530

    kalyan log

---
+++ 11.1-logs.sh
@@ -0,0 +1,30 @@
+
+ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+VALIDATE(){
+  if [ $1 -ne 0 ]
+  then
+    echo " error : $2 FAILED"
+    exit 1
+    else
+      echo "$2 SUCCESS"
+      fi
+}
+
+if [ $ID -ne 0 ]
+then
+  echo "Error: Please run with root access"
+  else
+    exit 1
+    echo "you are the root user"
+    fi
+
+    yum install mysql -y &>> $LOGFILE
+
+    validate $? "Installation MySQL"
+
+    yum install git -y &>> $LOGFILE
+    validate $? "installed git"
