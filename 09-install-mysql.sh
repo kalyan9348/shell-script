ba28f502b22c9f1454dc2dc24fa2bfafffeb3b76
Kalyan Reddy <kalyanreddy5030@gmail.com>
2023-12-16 14:29:04 +0530

    installing mysql

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -1,3 +1,11 @@
 #Install MySQL through shell script
 
-yum install mysqlll -y
+ID=$(id -u)
+if [ $ID -ne 0 ]
+then
+  echo "ERROR:: Please run the script with root access"
+  else
+    echo "you are the root user"
+fi
+
+yum install mysql-y
