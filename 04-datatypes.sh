4b99153bc5f1e74c65006ed560be9d63ff9d2187
Kalyan Reddy <kalyanreddy5030@gmail.com>
2023-12-16 08:20:18 +0530

    added 2nd script

--- 04-datatypes.sh
+++ 04-datatypes.sh
@@ -1,15 +1,22 @@
-#NUMBER1=$1
-#NUMBER2=$2
+NUMBER1=$1
+NUMBER2=$2
 
-#SUM=$(($NUMBER1+$NUMBER2))
+SUM=$(($NUMBER1+$NUMBER2))
 
-#echo "Total:: $SUM"
+echo "Total:: $SUM"
 
-#Syntax to run this sh 04-datatypes.sh 10 20
+Syntax to run this sh 04-datatypes.sh 10 20
 
-
+#With Arguments
 a=$1
 b=$2
 
-sum=$(($1+$2))
+sum=$(($1 + $2))
+echo "Total= $sum"
+
+#without Arguments
+
+a=10
+b=20
+sum=((a+b))
 echo "Total= $sum"
