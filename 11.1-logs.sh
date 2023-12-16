a71b9facd9fc71256f25872835891c670d8ad111
Kalyan Reddy <kalyanreddy5030@gmail.com>
2023-12-16 22:24:40 +0530

    kalyan log

--- 11.1-logs.sh
+++ 11.1-logs.sh
@@ -3,23 +3,26 @@ ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 VALIDATE(){
   if [ $1 -ne 0 ]
   then
-    echo " error : $2 FAILED"
+    echo " error : $2 .... $R FAILED $N"
     exit 1
     else
-      echo "$2 SUCCESS"
+      echo "$2 .... $G SUCCESS $N"
       fi
 }
 
 if [ $ID -ne 0 ]
 then
-  echo "Error: Please run with root access"
+  echo -e "$R Error: Please run with root access"
   exit 1
   else
-    echo "you are the root user"
+    echo "$G you are the root user"
     fi
 
     yum install mysql -y &>> $LOGFILE
