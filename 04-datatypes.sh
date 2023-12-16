fab09e33eb424d75b9a23c31c6dcc17594929bcf
Kalyan Reddy <kalyanreddy5030@gmail.com>
2023-12-16 08:12:47 +0530

    added 2nd script

--- 04-datatypes.sh
+++ 04-datatypes.sh
@@ -1,9 +1,9 @@
-NUMBER1=$1
-NUMBER2=$2
+#NUMBER1=$1
+#NUMBER2=$2
 
-SUM=$(($NUMBER1+$NUMBER2))
+#SUM=$(($NUMBER1+$NUMBER2))
 
-echo "Total:: $SUM"
+#echo "Total:: $SUM"
 
 #Syntax to run this sh 04-datatypes.sh 10 20
 
