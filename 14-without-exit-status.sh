449cd2150e668a384919de742b9313765ba92773
Kalyan Reddy <kalyanreddy5030@gmail.com>
2023-12-17 20:39:56 +0530

    installing packages using for loop

---
+++ 14-without-exit-status.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+ls -ltr
+
+touch example.txt
+
+echo "Before wrong command" #after this command we will be getting error since the below command was error
+
+lsfff
+
+echo "After wrong command"
+
+cd /tmp
+
+cd /home/centos
