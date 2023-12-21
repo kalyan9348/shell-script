86e920510359647360b9fdc30dd457c648dcfb01
Kalyan Reddy <kalyanreddy5030@gmail.com>
2023-12-21 14:05:22 +0530

    roboshop shell configuration

--- 03-variables.sh
+++ 03-variables.sh
@@ -12,7 +12,7 @@ echo "Username is:$USERNAME, Password is:$PASSWORD"
 
 echo "Please enter your username:: "
 
-read -s USERNAME #the value entered above will be automatically atteched to USERNAME variable
+read -s USERNAME #the value entered above will be automatically attached to USERNAME variable
 
 echo "Please enter your Password:: "
 read -s PASSWORD
